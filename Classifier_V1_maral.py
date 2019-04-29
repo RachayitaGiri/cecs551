cf4617db681441015e9cab57cfc01689a7e8eb95
Maral <kasiri@usc.edu>
2019-04-29 15:22:56 -0700

    maral update3

--- Classifier_V1_maral.py
+++ Classifier_V1_maral.py
@@ -34,7 +34,10 @@ import progressbar as pb
 
 with open('/home/datasets/annotations/train_labels.csv' ) as csvfile:
     labels= csv.reader(csvfile, delimiter= ',')
-    
+    for i in labels:
+        print(i[0])
+        print(type(i))
+        
 print(type(labels))
 '''
 image_list=[]
