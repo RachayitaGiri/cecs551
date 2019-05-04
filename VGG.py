bfb75187ac641c7fc09cf22a17917e2b90c177bc
Chris <chriscervola@gmail.com>
2019-05-04 15:25:00 -0700

    update relu call

--- VGG.py
+++ VGG.py
@@ -21,72 +21,59 @@ vggModel = Sequential()
 
 # Convolution with 64 filters of size 3x3
 vggModel.add(ZeroPadding2D((1,1)))
-vggModel.add(Conv2D(64, (3,3), strides = (1,1), input_shape=(224,224,3)))
-vggModel.add(Activation('relu'))
+vggModel.add(Conv2D(64, (3,3), strides = (1,1), activation = 'relu', input_shape=(224,224,3)))
 
 vggModel.add(ZeroPadding2D((1,1)))
-vggModel.add(Conv2D(64, (3,3), strides = (1,1)))
-vggModel.add(Activation('relu'))
+vggModel.add(Conv2D(64, (3,3), strides = (1,1), activation = 'relu'))
 
 # 2x2 filter with stride 2
-vggModel.add(MaxPooling2D(pool_size=(2, 2)))
+vggModel.add(MaxPooling2D(pool_size=(2, 2), ))
 
 # Convolution with 128 filters of size 3x3
 vggModel.add(ZeroPadding2D((1,1)))
-vggModel.add(Conv2D(128, (3,3), strides = (1,1)))
-vggModel.add(Activation('relu'))
+vggModel.add(Conv2D(128, (3,3), strides = (1,1), activation = 'relu'))
 
 vggModel.add(ZeroPadding2D((1,1)))
-vggModel.add(Conv2D(128, (3,3), strides = (1,1)))
-vggModel.add(Activation('relu'))
+vggModel.add(Conv2D(128, (3,3), strides = (1,1), activation = 'relu'))
 
 # 2x2 filter with stride 2
 vggModel.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Convolution with 256 filters of size 3x3
 vggModel.add(ZeroPadding2D((1,1)))
-vggModel.add(Conv2D(256, (3,3), strides = (1,1)))
-vggModel.add(Activation('relu'))
+vggModel.add(Conv2D(256, (3,3), strides = (1,1), activation = 'relu'))
 
 vggModel.add(ZeroPadding2D((1,1)))
-vggModel.add(Conv2D(256, (3,3), strides = (1,1)))
-vggModel.add(Activation('relu'))
+vggModel.add(Conv2D(256, (3,3), strides = (1,1), activation = 'relu'))
 
 vggModel.add(ZeroPadding2D((1,1)))
-vggModel.add(Conv2D(256, (3,3), strides = (1,1)))
-vggModel.add(Activation('relu'))
+vggModel.add(Conv2D(256, (3,3), strides = (1,1), activation = 'relu'))
 
 # 2x2 filter with stride 2
 vggModel.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Convolution with 512 filters of size 3x3
 vggModel.add(ZeroPadding2D((1,1)))
-vggModel.add(Conv2D(512, (3,3), strides = (1,1)))
-vggModel.add(Activation('relu'))
+vggModel.add(Conv2D(512, (3,3), strides = (1,1), activation = 'relu'))
 
 vggModel.add(ZeroPadding2D((1,1)))
-vggModel.add(Conv2D(512, (3,3), strides = (1,1)))
-vggModel.add(Activation('relu'))
+vggModel.add(Conv2D(512, (3,3), strides = (1,1), activation = 'relu'))
 
 vggModel.add(ZeroPadding2D((1,1)))
-vggModel.add(Conv2D(512, (3,3), strides = (1,1)))
-vggModel.add(Activation('relu'))
+vggModel.add(Conv2D(512, (3,3), strides = (1,1), activation = 'relu'))
 
 # 2x2 filter with stride 2
 vggModel.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Convolution with 512 filters of size 3x3
 vggModel.add(ZeroPadding2D((1,1)))
-vggModel.add(Conv2D(512, (3,3), strides = (1,1)))
-vggModel.add(Activation('relu'))
+vggModel.add(Conv2D(512, (3,3), strides = (1,1), activation = 'relu'))
 
 vggModel.add(ZeroPadding2D((1,1)))
-vggModel.add(Conv2D(512, (3,3), strides = (1,1)))
-vggModel.add(Activation('relu'))
+vggModel.add(Conv2D(512, (3,3), strides = (1,1), activation = 'relu'))
 
 vggModel.add(ZeroPadding2D((1,1)))
-vggModel.add(Conv2D(512, (3,3), strides = (1,1)))
-vggModel.add(Activation('relu'))
+vggModel.add(Conv2D(512, (3,3), strides = (1,1), activation = 'relu'))
 
 # 2x2 filter with stride 2
 vggModel.add(MaxPooling2D(pool_size=(2, 2)))
@@ -94,12 +81,10 @@ vggModel.add(MaxPooling2D(pool_size=(2, 2)))
 # Need FC layers (2)
 vggModel.add(Flatten())
 
-vggModel.add(Dense(4096))
-vggModel.add(Activation('relu'))
+vggModel.add(Dense(4096, activation = 'relu'))
 vggModel.add(Dropout(0.5))
 
-vggModel.add(Dense(4096))
-vggModel.add(Activation('relu'))
+vggModel.add(Dense(4096, activation = 'relu'))
 vggModel.add(Dropout(0.5))
 
 # Need FC Layer (1 for each class)
