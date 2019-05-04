b20ec8840e8769d978bb304298de7974ea6a0ced
Rachayita Giri <rachayitagiri@gmail.com>
2019-05-04 13:44:04 -0700

    Update coco_dataset.py to return subsets

--- coco_dataset.py
+++ coco_dataset.py
@@ -23,3 +23,23 @@ def load_data():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
     return (X_train, X_test, y_train, y_test)
+
+'''
+    Function to return the subset of train and test sets: 80-20 split
+    Output:
+        X_train: (Ntr, 224x224x3)
+        X_test:  (Nte, 224x224x3)
+        y_train: (Ntr, 1)
+        y_test:  (Nte, 1)
+    where:
+        Ntr = number of rows in the training set
+        Nte = number of rows in the test set
+'''
+def load_data_subset():
+    labels = pd.read_csv('/home/datasets/annotations/val_labels.csv', delimiter=',', nrows=500)
+    X = pd.read_csv('/home/datasets/img2array.csv', delimiter=',', nrows=500)
+    y = labels[:, 1:]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    return (X_train, X_test, y_train, y_test)
