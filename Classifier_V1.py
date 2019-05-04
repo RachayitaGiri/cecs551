b90706d1ae95316e83252c6afc08a0d92a9410f9
Maral Kasiri <kasiri@usc.edu>
2019-05-04 16:09:56 -0700

    Update Classifier_V1.py
    subset run

--- Classifier_V1.py
+++ Classifier_V1.py
@@ -12,7 +12,8 @@ import time
 
 start = time.time() 
 
-X_train, X_test, y_train, y_test = load_data()
+#X_train, X_test, y_train, y_test = load_data()
+X_train, X_test, y_train, y_test = load_data_subset()
 
     
 
