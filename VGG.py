07592bcab5749aab66493f9af13edb6576f2fa4a
Chris <chriscervola@gmail.com>
2019-05-04 14:47:32 -0700

    updated model to track hyperparamters in an output file

--- VGG.py
+++ VGG.py
@@ -1,3 +1,21 @@
+from __future__ import print_function
+import sys
+sys.path.append('../')
+from sklearn.metrics import confusion_matrix
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, BatchNormalization
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
+from statistics import mean
+from scripts.coco_dataset import load_data
+import time
+
+from keras.callbacks import TensorBoard
+
+start = time.time() 
+
+x_train, x_test, y_train, y_test = load_data()
+
 # VGGNet 16 Layer Implementation
 vggModel = Sequential()
 
@@ -91,4 +109,67 @@ vggModel.add(Dense(91))
 vggModel.add(Dense(1000))
 vggModel.add(Activation('sigmoid'))
 
-vggModel.summary()
+# Open the file to which the output will be written
+resfile = open("../outputs/VGG_output_test.txt","a")
+resfile.write("\n- - - - - - - - - - - - \nMODEL EXECUTION DETAILS |\n- - - - - - - - - - - -\n")
+
+# Compile the model
+vggModel.compile(
+    loss='categorical_crossentropy',
+    optimizer = Adam(lr=1e-4),
+    metrics=["accuracy"]
+)
+
+# use tensorboard to visualize our models
+#tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
+
+mystr = vggModel.summary()
+
+vggModel.summary(print_fn=lambda x: resfile.write(x + '\n'))
+
+# Train the model for the given number of epochs
+history = vggModel.fit(
+    x_train, y_train,
+    steps_per_epoch=10,
+    epochs=2,
+    verbose=1,
+    validation_data=(x_test, y_test),
+    validation_steps=10,
+    #callbacks=[tensorboard]
+)
+
+duration = time.time() - start
+
+# Write the results to a file
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+resfile.write("\nMean Training Loss = "+str(mean(loss)))
+resfile.write("\nMean Validation Loss = "+str(mean(val_loss)))
+resfile.write("\nMean Training Accuracy = "+str(mean(acc)))
+resfile.write("\nMean Validation Accuracy = "+str(mean(val_acc)))
+resfile.write("\nNumber of epochs, steps per epoch = "+str(len(loss))+", 10")
+resfile.write("\nTime taken = %s seconds" % duration)    
+resfile.write("\nLearning Rate = 1e-4")
+resfile.write("\nOptimizer = Adam\n")   
+
+""" # Evaluate the losses of the model 
+epochs = range(1, len(loss)+1)
+plt.plot(epochs, loss, color='red', label='Training Loss')
+plt.plot(epochs, val_loss, color='blue', label='Validation Loss')
+plt.title('Training and Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+# Evaluate the accuracy of the model
+plt.plot(epochs, acc, color='red', label='Training acc')
+plt.plot(epochs, val_acc, color='green', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+ """
