61a0965b2df1dfaf56f5338c62ff289d2968b4a2
Rachayita Giri <rachayitagiri@gmail.com>
2019-05-04 14:00:11 -0700

    Update coco_dataset.py

--- coco_dataset.py
+++ coco_dataset.py
@@ -24,6 +24,7 @@ def load_data():
 
     return (X_train, X_test, y_train, y_test)
 
+
 def load_data_subset():
     '''
     Function to return the subset of train and test sets: 80-20 split
@@ -36,10 +37,18 @@ def load_data_subset():
         Ntr = number of rows in the training set
         Nte = number of rows in the test set
     '''
+    # Read the dataset from the CSV file into a dataframe
     labels = pd.read_csv('/home/datasets/annotations/val_labels.csv', delimiter=',', nrows=500)
     X = pd.read_csv('/home/datasets/img2array.csv', delimiter=',', nrows=500)
-    y = labels[:, 1:]
+    y = labels.iloc[:, 1:]
 
+    # Split the dataset into train and test values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    # Reshape the data subsets into dimensions apt for the CNN, if need be
+    X_train = X_train.values
+    X_test = X_test.values
+    y_train = y_train.values
+    y_test = y_test.values 
 
     return (X_train, X_test, y_train, y_test)
