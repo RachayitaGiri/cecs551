22612e07a32fa208b25f73dc5d269d83f455a7a6
Stephanie <stephlopez915@gmail.com>
2019-05-04 15:28:46 -0700

    update relu call

--- AlexNet.py
+++ AlexNet.py
@@ -31,46 +31,39 @@ model_alexnet = Sequential()
 
 
 ##1.  11x11 convolution with 4x4 stride and 96 filters
-model_alexnet.add(Conv2D(96, (11, 11), strides = (4,4),
+model_alexnet.add(Conv2D(96, (11, 11), strides = (4,4), activation = 'relu',
                  input_shape=(224,224,3)))
-model_alexnet.add(Activation('relu'))
 
 model_alexnet.add(MaxPooling2D(pool_size=(3, 3), strides=(2,2)))
 
 ##2.  5x5 convolution with 1x1 stride and 256 filters
 model_alexnet.add(ZeroPadding2D((2,2)))
-model_alexnet.add(Conv2D(256, (5, 5), strides = (1,1)), padding=2)
-model_alexnet.add(Activation('relu'))
+model_alexnet.add(Conv2D(256, (5, 5), strides = (1,1)), padding=2, activation = 'relu')
 
 model_alexnet.add(MaxPooling2D(pool_size=(3, 3), strides=(2,2)))
 
 ##3.  3x3 convolution with 1x1 stride and 384 filters
 model_alexnet.add(ZeroPadding2D((1,1)))
-model_alexnet.add(Conv2D(384, (3, 3), strides = (1,1)))
-model_alexnet.add(Activation('relu'))
+model_alexnet.add(Conv2D(384, (3, 3), strides = (1,1), activation = 'relu'))
 
 ##4.  3x3 convolution with 1x1 stride and 384 filters
 model_alexnet.add(ZeroPadding2D((1,1)))
-model_alexnet.add(Conv2D(384, (3, 3), strides = (1,1))) 
-model_alexnet.add(Activation('relu'))
+model_alexnet.add(Conv2D(384, (3, 3), strides = (1,1), activation = 'relu')) 
 
 ##5.  3x3 convolution with 1x1 stride and 256 filters
 model_alexnet.add(ZeroPadding2D((1,1)))
-model_alexnet.add(Conv2D(256, (3, 3), strides = (1,1))) 
-model_alexnet.add(Activation('relu'))
+model_alexnet.add(Conv2D(256, (3, 3), strides = (1,1), activation = 'relu')) 
 
 model_alexnet.add(MaxPooling2D(pool_size=(3, 3), strides=(2,2)))
 
 
 ## 6. Flatten and dense
 model_alexnet.add(Flatten())
-model_alexnet.add(Dense(4096))
-model_alexnet.add(Activation('relu'))
+model_alexnet.add(Dense(4096, activation = 'relu'))
 model_alexnet.add(Dropout(0.5))
 
 #7.  dense
-model_alexnet.add(Dense(4096))
-model_alexnet.add(Activation('relu'))
+model_alexnet.add(Dense(4096, activation = 'relu'))
 model_alexnet.add(Dropout(0.5))
 
 #8.  number of classes
